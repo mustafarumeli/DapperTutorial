c3acdb911121aa4d180d2523d2e4a89881630c1c
Mustafa RUMELİ <mustafarumeli@outlook.com>
2019-08-08 17:05:39 +0300

    Dapper First Try

---
+++ DapperTutorial/DapperTable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DapperTutorial
+{
+    public class DapperTable
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DapperTable()
+        {
+            Id = Guid.NewGuid();
+        }
+        public override string ToString()
+        {
+            return $"Name = {Name} BirthDate = {BirthDate} Id = {Id}";
+        }
+    }
+}
--- DapperTutorial/Program.cs
+++ DapperTutorial/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-
+using Dapper;
 namespace DapperTutorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dapperCurd = new DapperCrud<DapperTable>();
+            dapperCurd.Insert(new DapperTable { Name = "Mustafa", BirthDate = new DateTime(1995, 03, 21) });
+            foreach (DapperTable item in dapperCurd.List)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
